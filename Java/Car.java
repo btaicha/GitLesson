ca5dd75f822523f3df52dd33b935eb4dc0c6f630
Aicha B <aicha.bard@gmail.com>
2017-12-01 10:14:15 +0100

    dossier car

---
+++ Java/Car.java
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * @author HB1 Aicha
+ *
+ */
+public class Car {
+    /**
+     *
+     */
+    private String color;
+
+    /**** @return car */
+    public String getCar() {
+        return color;
+    }
+
+    /**
+     *
+     * @param car1
+     *            
+     */
+    public void setCar(final String car1) {
+        this.color = car1;
+    }
+
+}
